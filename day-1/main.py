feaff7bd66bc56a48b1e9d5b45f7ad8de8547fdf
saminmahmud <samin.mahmud18@gmail.com>
2024-09-29 11:46:58 +0600

    Add day-1

---
+++ day-1/main.py
@@ -0,0 +1,10 @@
+# Working with Variables in Python to Manage Data
+
+# Project: Band Name Generator
+
+print("Welcome to the Band Name Generator.")
+
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of a pet?\n")
+
+print(f"Your band name could be {city} {pet}")
